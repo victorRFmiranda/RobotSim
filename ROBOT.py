ef74249bc0b219ab5a2a367a6096af87e40354d3
victor <victormrfm@gmail.com>
2024-03-17 18:18:59 -0300

    .

--- ROBOT.py
+++ ROBOT.py
@@ -1,14 +1,28 @@
-
 import pygame
 import math
 import numpy as np
+import random
+
+import grid_map
+import map_utils
 
 MAP_SCALE=10
 
-def distance(point1, point2):
+def distance(point1:float , point2:float) -> float:
     point1 = np.array(point1)
     point2 = np.array(point2)
     return np.linalg.norm(point1-point2)
+
+def _difference_two_angles(angle1: float, angle2: float) -> float:
+    diff = (angle1 - angle2) % (math.pi * 2)
+    if diff >= math.pi:
+        diff -= math.pi * 2
+    return diff
+
+def _orientation_robot_to_end(point1: float, point2: float) -> float:
+    x = point1[0] - point2[0]
+    y = point1[1] - point2[1]
+    return math.atan2(y, x)
 
 # Função para converter de metros para pixels
 def meters_to_pixels(meters):
@@ -18,12 +32,12 @@ def pixels_to_meter(pixels):
     return pixels/MAP_SCALE
 
 class Robot:
-    def __init__(self, startpos, width):
+    def __init__(self, startpos):
 
         # self.m2p = 3779.52  # from meters to pixels
         self.m2p = 10  # from meters to pixels
-        # robot dims
-        self.w = width
+        # robot axis dist
+        self.w = 1
 
         self.x = startpos[0]*self.m2p
         self.y = startpos[1]*self.m2p
@@ -32,12 +46,22 @@ class Robot:
         self.min_obs_dist = 1
         self.count_down = 5 # seconds
 
+        self.linear_velocity = 0.0
+        self.angular_velocity = 0.0
+
     def get_pose(self):
         pose = [pixels_to_meter(self.x),pixels_to_meter(self.y),self.heading]
 
         return pose
 
-    def check_collision(self, point_cloud, dt):
+    def get_vel(self):
+
+        linear_velocity = self.linear_velocity + random.uniform(-0.02, 0.02)
+        angular_velocity = self.angular_velocity + random.uniform(-0.02, 0.02)
+
+        return linear_velocity, angular_velocity
+
+    def check_collision(self, point_cloud):
         closest_obs = None
         dist = np.inf
 
@@ -55,7 +79,26 @@ class Robot:
         else:
             return True
 
-    def kinematics(self, v, w, dt):
+
+    def check_collision2(self, point_cloud, obs_dist):
+        closest_obs = None
+        dist = np.inf
+
+        if len(point_cloud) > 1:
+            for point in point_cloud:
+                if dist > distance([self.x,self.y], point):
+                    dist = distance([self.x,self.y], point)
+                    closest_obs = (point,dist)
+
+            if closest_obs[1] < obs_dist*self.m2p:
+                return False
+            else:
+                return True
+
+        else:
+            return True
+
+    def step(self, v, w, dt):
         b = 0.5
 
         self.vr = v*self.m2p + (w*self.m2p*b / 2)
@@ -68,9 +111,12 @@ class Robot:
         if self.heading>2*math.pi or  self.heading<-2*math.pi:
             self.heading = 0
 
+        self.linear_velocity = v
+        self.angular_velocity = w
+
 
 class Graphics:
-    def __init__(self, map_scale, dimentions, robot_img_path, map_img_path):
+    def __init__(self, dimentions, robot_img_path, map_img_path):
         pygame.init()
         # COLORS
         self.black = (0, 0, 0)
@@ -84,11 +130,13 @@ class Graphics:
 
         # load imgs
         self.robot = pygame.image.load(robot_img_path)
-        self.map_img = pygame.image.load(map_img_path)
+        map_img2 = pygame.image.load(map_img_path)
+        self.map_img = pygame.transform.scale(map_img2, (500, 500))
 
         # dimentions
-        self.height, self.width = dimentions
-        self.map_scale = map_scale
+        self.height = self.map_img.get_width()
+        self.width = self.map_img.get_height()
+        self.map_scale = self.width/dimentions
 
         # window settings
         pygame.display.set_caption("Reinforcement Learning")
@@ -104,24 +152,26 @@ class Graphics:
         for point in point_cloud:
             pygame.draw.circle(self.map, self.red, point, 3, 0)
 
+    def draw_target(self, pos):
+        n_pos = (meters_to_pixels(pos[0]),meters_to_pixels(pos[1]))
+        pygame.draw.circle(self.map, self.green, n_pos, 5)
+
 
     def get_mapObsFree(self):
         obstacles = []
         free_areas = []
         # Obtém os pixels da imagem como uma matriz numpy
         pixels = np.array(pygame.surfarray.array2d(self.map_img))
-        # Percorra os pixels da imagem para criar obstáculos e áreas livres
+         # Percorra os pixels da imagem para criar obstáculos e áreas livres
         for x in range(self.width):
             for y in range(self.height):
                 if pixels[x][y] == -1:  # Verifique se o pixel é preto (0)
                     free_areas.append((x, y))
-                    # obstacles.append((x, y))
                 else:
-                # elif pixels[x][y] == 255:  # Verifique se o pixel é branco (255)
-                    # free_areas.append((x, y))
                     obstacles.append((x, y))
 
         return  obstacles, free_areas
+
             
 
 class LiDAR:
@@ -162,13 +212,152 @@ class LiDAR:
 
 
 
+    # ########################################
+    # '''         Compute Map         '''
+    # ########################################
+    # def get_map(self):
+
+    #     map_increase = 0.0
+
+    #     lidar = lidar_scan
+    #     pose = [self._env.get_robot_pose_x(),self._env.get_robot_pose_y(),self._env.get_robot_pose_orientation()]
+    #     # pmap,map_increase  = self.Map.update_map(lidar,pose)
+
+    #     ang_step = 0.00436332312998582394230922692122153178360717972135431364024297860042752278650862360920560392408627370553076123126
+    #     angle_min = -2.3561944902
+    #     angle_max = 2.3561944902
+    #     range_min = 0.06
+    #     range_max = 20
+    #     angles = np.arange(angle_min,angle_max,ang_step)
+
+    #     alpha = 1.0  # delta for max rang noise
+
+    #     # Lidar measurements in X-Y plane
+    #     distances_x, distances_y = lidar_scan_xy(lidar, angles, pose[0], pose[1], pose[2])
+
+    #     # x1 and y1 for Bresenham's algorithm
+    #     x1, y1 = self.gridMap.discretize(pose[0], pose[1])
+
+    #     # for BGR image of the grid map
+    #     X2 = []
+    #     Y2 = []
+
+    #     for (dist_x, dist_y, dist) in zip(distances_x, distances_y, lidar):
+
+    #         # x2 and y2 for Bresenham's algorithm
+    #         x2, y2 = self.gridMap.discretize(dist_x, dist_y)
+
+    #         # draw a discrete line of free pixels, [robot position -> laser hit spot)
+    #         for (x_bres, y_bres) in bresenham(self.gridMap, x1, y1, x2, y2):
+
+    #             self.gridMap.update(x = x_bres, y = y_bres, p = self.P_free)
+
+    #         # mark laser hit spot as ocuppied (if exists)
+    #         if dist < range_max - alpha:
+                
+    #             self.gridMap.update(x = x2, y = y2, p = self.P_occ)
+
+    #         # for BGR image of the grid map
+    #         X2.append(x2)
+    #         Y2.append(y2)
+
+    #     # converting grip map to BGR image
+    #     # bgr_image = self.gridMap.to_BGR_image()
+
+    #     gray_image, map_increase = self.gridMap.to_grayscale_image()
+
+    #     # set_pixel_color(bgr_image, x1, y1, 'BLUE')
+            
+    #     # # # marking neighbouring pixels with blue pixel value 
+    #     # for (x, y) in self.gridMap.find_neighbours(x1, y1):
+    #     #     set_pixel_color(bgr_image, x, y, 'BLUE')
+
+    #     # # marking laser hit spots with green value
+    #     # for (x, y) in zip(X2,Y2):
+    #     #     set_pixel_color(bgr_image, x, y, 'GREEN')
+
+
+    #     resized_image = cv2.resize(gray_image,(100, 100),interpolation = cv2.INTER_NEAREST)
+
+
+    #     rotated_image = cv2.rotate(src = gray_image, 
+    #                    rotateCode = cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+
+    #     # cv2.imshow('Mapa',rotated_image)
+    #     # cv2.waitKey(0)
+    #     # cv2.destroyAllWindows()
+
+
+    #     pmap = rotated_image
+
+    #     return pmap, map_increase
 
 
 
+class Environment:
+
+    def __init__(self, dimentions, map_img_path):
+        self.gfx = Graphics(dimentions,'DDR.png','world/mapa_2.png')
+        start = (10,10)
+        self.robot = Robot(start)
+        sensor_range = 5, math.radians(135)
+        self.lidar = LiDAR(sensor_range, self.gfx.map)
+        self.point_cloud = []
+        self.scan = []
+        self.dt = 0.01
+        self.target_pos = (0,0)
+
+        obs, self.free_areas = self.gfx.get_mapObsFree()
+
+    def reset(self, dt):
+        done = False
+        while not done:
+            new_pixel = random.choice(self.free_areas)
+            new_pos = (pixels_to_meter(new_pixel[0]),pixels_to_meter(new_pixel[1]))
+            self.robot = Robot(new_pos)
+            done,_ = self.step(0,0,dt)
+            done = self.robot.check_collision2(self.point_cloud, 3)
+            t_pos = random.choice(self.free_areas)
+            self.target_pos = (pixels_to_meter(t_pos[0]),pixels_to_meter(t_pos[1]))
 
 
+    def step(self, liner_vel, ang_vel,dt):
 
+        # update map
+        self.gfx.map.blit(self.gfx.map_img,(0,0))
 
+        # robot step
+        self.robot.step(liner_vel,ang_vel,dt)
+
+        # extract point_cloud and lidar scan
+        self.point_cloud, self.scan = self.lidar.sense_obstacles(self.robot.x, self.robot.y, self.robot.heading)
+
+        # draw liDAR
+        self.gfx.draw_sensor_data(self.point_cloud)
+
+        # draw Robot
+        self.gfx.draw_robot(self.robot.x, self.robot.y, self.robot.heading)
+        done = self.robot.check_collision(self.point_cloud)
+
+        # draw target
+        self.gfx.draw_target(self.target_pos)
+
+        # define observation
+        robot_pose = self.robot.get_pose()
+        dist_to_goal = distance(robot_pose[0:2],self.target_pos)
+        angle_to_goal = _orientation_robot_to_end(self.target_pos,robot_pose[0:2])
+        diff_angle = _difference_two_angles(robot_pose[2], angle_to_goal)
+        lin_vel, ang_vel = self.robot.get_vel()
+
+        observation = self.scan[:,0].tolist()
+        observation.append(dist_to_goal)
+        observation.append(diff_angle)
+        observation.append(lin_vel)
+        observation.append(ang_vel)
+
+        return done, observation
+        
 
 
 
--- main.py
+++ main.py
@@ -1,96 +1,38 @@
 import math
 import pygame
-from ROBOT import Graphics,Robot,LiDAR
+from ROBOT import Environment,Robot,LiDAR
 
 
-# MAP_DIMENSIONS = (600, 1200)
-MAP_DIMENSIONS = (500, 500)
-MAP_SCALE = 10
-
+# Max  x == y (m), need to be equals
+MAP_DIMENSIONS = 50
 
 # the environment graphics
-gfx = Graphics(MAP_SCALE, MAP_DIMENSIONS,'DDR.png','world/mapa.png')
-
-# the robot
-start = (10,10)
-robot=Robot(start,10)
-
-
-# the sensor   (range [m], deg)
-sensor_range = 5, math.radians(135)
-laser = LiDAR(sensor_range, gfx.map)
+env = Environment(MAP_DIMENSIONS,'world/mapa_2.png')
 
 dt=0
 last_time = pygame.time.get_ticks()
 
 running = True
 
-obstacles, freeSpace = gfx.get_mapObsFree()
-
+First_run = True
 # simulation loop
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
     dt=(pygame.time.get_ticks()-last_time)/1000
     last_time = pygame.time.get_ticks()
 
-    # start world
-    gfx.map.blit(gfx.map_img,(0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                env.reset(dt)
+
+    if First_run:
+        env.reset(dt)
+        First_run = False
 
     # Robot kinematics [v (m/s), w(rad/s), dt]
-    robot.kinematics(0,0.5,dt)
-
-    # extract point_cloud and lidar scan
-    point_cloud, scan = laser.sense_obstacles(robot.x, robot.y, robot.heading)
-
-    # draw liDAR
-    gfx.draw_sensor_data(point_cloud)
-
-    # draw Robot
-    gfx.draw_robot(robot.x, robot.y, robot.heading)
-
-    # Stop sim if collision is True
-    running = robot.check_collision(point_cloud, dt)
-
+    running, obs = env.step(0,0.5,dt)
+        
 
     pygame.display.update()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
